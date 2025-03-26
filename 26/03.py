21ad65902c0b93d7872cc91362b4317e1dea005b
Tu Nombre <tuemail@example.com>
2025-03-26 06:58:29 -0500

    34

---
+++ 26/03.py
@@ -0,0 +1,20 @@
+#Desarrollar un programaen Python que permita gestionar productos de apoyo para perso nas con discapacidad o necesidades especiales. El sistema debe permitir el registro, consulta y carga de productos desde un archivo CSV, con validación de datos y registro de eventos mediante logging.
+logging.basicConfig(level=logging.INFO, filename='productos.log',
+                    level=logging.DEBUG,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+                    
+
+def validar_string():
+    pass
+
+def registrar_producto():
+    codigo = validar_string
+    nombre = validar_string
+    CATEGORIA = ['movilidad', 'comunicacion', 'vision', 'audicion']
+    pass
+
+def cargar_csv ():
+    pass
+
+def mostrar_producto():
+    pass
